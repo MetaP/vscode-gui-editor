f5a21369930f717b805ab7c6718bd99f29665d31
Paul Gordts <paul.gordts@live.com>
2023-03-25 18:19:50 +0100

    Angular application gui with demo component added

---
+++ gui/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'metap-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'gui';
+}
